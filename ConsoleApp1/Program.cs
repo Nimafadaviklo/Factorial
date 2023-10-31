92cb74a9128563c3ee61dfdbe8a13e017bed41d2
Acer <Acer@DESKTOP-JS0O9TD>
2023-10-31 23:28:50 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿//نیما فدوی
+//فاکتوریل
+int f = 1, num;
+Console.WriteLine("enter numbre");
+num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=
+    num; i++)
+{
+    if (num == 0) break;
+    f *= i;
+}
+Console.WriteLine("{0}!={1}", num, f);
+Console.ReadKey();
